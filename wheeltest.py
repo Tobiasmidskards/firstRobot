1e1bf3762c8a97d30fe03401e702e3d734e979a7
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-08 02:56:56 +0200

    Added for loop instead of defining a function
    Added print

--- wheeltest.py
+++ wheeltest.py
@@ -7,20 +7,24 @@ GPIO.setup(11,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
 
-def wheeltest():
+for x in range (0,1)
   GPIO.output(7,True)
+  print "Motor 1-7 is running"
   time.sleep(1)
   GPIO.output(7,False)
   GPIO.output(11,True)
+  print "Motor 2-11 is running"
   time.sleep(1)
   GPIO.output(11,False)
   GPIO.output(13,True)
+  print "Motor 3-13 is running"
   time.sleep(1)
   GPIO.output(13,False)
   GPIO.output(15,True)
+  print "Motor 4-15 is running"
   time.sleep(1)
   GPIO.output(15,False)
-  GPIO.cleanup()
-  return
   
 GPIO.cleanup()
+
+print "Test is complete."
