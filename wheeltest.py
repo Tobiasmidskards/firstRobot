1531fe073cc1b617a3f3afc67c5d5d3588ceb228
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-08 23:56:13 +0200

    Update wheeltest.py

--- wheeltest.py
+++ wheeltest.py
@@ -7,7 +7,7 @@ GPIO.setup(11,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
 
-for x in range (0,1)
+for x in range (0,1):
   GPIO.output(7,True)
   print "Motor 1-7 is running"
   time.sleep(1)
