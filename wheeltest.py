70bba0dfa0d0d7335ac02b2d509e968d7bae29d4
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-08 02:46:38 +0200

    wheeltest.py
    Playing with Python and GPIO on the RaspberryPi

---
+++ wheeltest.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
+
+def wheeltest():
+  GPIO.output(7,True)
+  time.sleep(1)
+  GPIO.output(7,False)
+  GPIO.output(11,True)
+  time.sleep(1)
+  GPIO.output(11,False)
+  GPIO.output(13,True)
+  time.sleep(1)
+  GPIO.output(13,False)
+  GPIO.output(15,True)
+  time.sleep(1)
+  GPIO.output(15,False)
+  GPIO.cleanup()
+  return
+  
+GPIO.cleanup()
