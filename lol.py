3452c579fec1f9b6562566c14076bef3c5e29f64
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-12 01:50:47 +0200

    Create lol.py

---
+++ lol.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
+
+def ost():
+  GPIO.output(7,True)
+  time.sleep(1)
+  GPIO.output(7,False)
+  time.sleep
+
+for x in range (0,10):
+  ost()
+  print "test" x
+GPIO.cleanup()
+
+print "Test is complete."
