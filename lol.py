b51dabba89a17ced0bd53c58b5fc045d3711868c
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-12 02:15:56 +0200

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+from random import randint as rint
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
@@ -20,12 +21,22 @@ def blink():
   
 def onforten():
   GPIO.output(7,True)
-  print "I am on for 10 seconds"
-  time.sleep(10)
+  print "I am on for 4 seconds"
+  time.sleep(4)
   GPIO.output(7,False)
+  print "I am OFF"
+  
+def random():
+  for x in range (rint(0,10)):
+    GPIO.output(7,True)
+    print "I am blinking -",x,"out of",y
+    time.sleep(.5)
+    GPIO.output(7,False)
+    time.sleep(.5)
   print "I am OFF"
 
 blink()
+random()
 onforten()
 #skrivet = input ("Skriv et tal: ")
 
