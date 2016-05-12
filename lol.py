e37705ec8f69d7d3a358aba2a86055ebaa0ace53
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-12 02:09:16 +0200

    Update lol.py

--- lol.py
+++ lol.py
@@ -7,21 +7,32 @@ GPIO.setup(11,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
 
-def ost():
+def blink():
+  for x in range (0,10):
+    GPIO.output(7,True)
+    print "I am blinking"
+    time.sleep(.5)
+    GPIO.output(7,False)
+    time.sleep(.5)
+  print "I am OFF"
+  
+def onforten():
   GPIO.output(7,True)
-  print "lol"
-  time.sleep(1)
+  print "I am on for 10 seconds"
+  time.sleep(10)
   GPIO.output(7,False)
-  time.sleep(1)
+  print "I am OFF"
 
-skrivet = input ("Skriv et tal: ")
+blink()
+onforten()
+#skrivet = input ("Skriv et tal: ")
 
-for x in range (0,1):
-  if skrivet == 1:
-    print x+1
-    ost()
-  else:
-    print "skriv 1"
+#for x in range (0,1):
+#  if skrivet == 1:
+#    print x+1
+#    ost()
+#  else:
+#    print "skriv 1"
 
 GPIO.cleanup()
 
