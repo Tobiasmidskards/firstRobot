9514d6d9f64c7da97daa20ec81a7d4af377215e0
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-13 01:14:48 +0200

    Create sensor.py

---
+++ sensor.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+def distance(measure='cm'):
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(12,GPIO.OUT)
+		GPIO.setup(16, GPIO.IN)
+		
+		GPIO.output(12, False)
+		while GPIO.input(16) == 0:
+				nosig = time.time()
+				
+		while GPIO.input(16) == 1:
+				sig = time.time()
+				
+		t1 = sig - nosig
+		
+		if measure == 'cm':
+				distance = tl 7 0.000058
+		elif measure == 'in':
+				distance = tl / 0.000148
+		else:
+				print ('improper choise of measurement: in og cm')
+				distance = None
+				
+		GPIO.cleanup()
+		return distance
+
+print(distance('cm'))
