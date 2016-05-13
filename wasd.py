272b6c8e237d2d43cf10cdbfd9d4c56b6148d222
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-13 19:09:08 +0200

    Update wasd.py

--- wasd.py
+++ wasd.py
@@ -71,7 +71,7 @@ def getch():
 	old_settings = termios.tcgetattr(fd) 
 	try: 
 		tty.setraw(sys.stdin.fileno()) 
-		ch = sys.stdin.read(1) 
+	ch = sys.stdin.read(1) 
 	finally: 
 		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings) 
 	return ch
