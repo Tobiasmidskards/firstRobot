67508da35e824a3c266621941ca3e44e493db61c
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-14 04:02:52 +0200

    Update wasd.py

--- wasd.py
+++ wasd.py
@@ -82,34 +82,33 @@ time.sleep(.5)
 print("'K' Will stop the drive motors") 
 
 def on():
+	while True: 
+		GPIO.setmode(GPIO.BOARD) 
+		GPIO.setup(7, GPIO.OUT) 
+		GPIO.setup(11, GPIO.OUT) 
+		GPIO.setup(13, GPIO.OUT) 
+		GPIO.setup(15, GPIO.OUT)
+		GPIO.setup(18, GPIO.OUT)
+		GPIO.setup(40, GPIO.OUT)
 	
-   while True: 
-	GPIO.setmode(GPIO.BOARD) 
-	GPIO.setup(7, GPIO.OUT) 
-	GPIO.setup(11, GPIO.OUT) 
-	GPIO.setup(13, GPIO.OUT) 
-	GPIO.setup(15, GPIO.OUT)
-	GPIO.setup(18, GPIO.OUT)
-	GPIO.setup(40, GPIO.OUT)
-	
-	char = getch() 
-	if(char == "w"): 
-		forward() 
-	if(char == "a"): 
-		left() 
-	if(char == "s"): 
-		back() 
-	if(char == "d"): 
-		right() 
-	if(char == "k"): 
-		stahp() 
-	if(char == "b"): 
-		led()
-	if(char == "o"): 
-		ledon()
-	if(char == "p"): 
-		ledoff()
-	time.sleep(.02) 
+		char = getch() 
+		if(char == "w"): 
+			forward() 
+		if(char == "a"): 
+			left() 
+		if(char == "s"): 
+			back() 
+		if(char == "d"): 
+			right() 
+		if(char == "k"): 
+			stahp() 
+		if(char == "b"): 
+			led()
+		if(char == "o"): 
+			ledon()
+		if(char == "p"): 
+			ledoff()
+		time.sleep(.02) 
 
 
 print "cleanup"
