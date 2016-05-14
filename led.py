634cf47cd97c29d180932673a4c4f01bb1a3049e
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-14 03:42:50 +0200

    Update led.py

--- led.py
+++ led.py
@@ -3,9 +3,12 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(40, GPIO.OUT)
 
 GPIO.output(18,True)
+GPIO.output(40,True)
 time.sleep(5)
 GPIO.output(18,False)
+GPIO.output(40,False)
 
 GPIO.cleanup()
