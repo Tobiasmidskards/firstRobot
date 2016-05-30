5b2388f82753f0262b0ed9793583684eb15afa7d
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-05-30 12:25:47 +0200

    Create pwm_test.py

---
+++ pwm_test.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(40, GPIO.OUT)
+
+p = GPIO.PWM(40, 50)
+
+print "starting motor"
+p.start(50)
+
+time.sleep(5)
+
+print "p.ChangeDutyCycle(90)"
+p.ChangeDutyCycle(90)
+
+time.sleep(5)
+
+print "p.ChangeFrequency(100)"
+p.ChangeFrequency(100)
+
+time.sleep(5)
+
+p.stop()
+
+GPIO.cleanup()
